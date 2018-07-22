834fb74c3c1d59f2574cc723fc956a2065c8f8e4
sakiko.adachi <lpe.6x3@gmail.com>
2018-07-22 16:49:21 +0900

    ラベルに表示はされるがOptionalと出るので要修正

--- SecondKadaiApp/ViewController.swift
+++ SecondKadaiApp/ViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var textField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +22,18 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // segueから遷移先のnameViewContollerを取得する
+        let NameViewController:nameViewController = segue.destination as! nameViewController
+        
+        NameViewController.name = textField.text
+        
+    }
+    
+    
+    @IBAction func unwind(_ segue: UIStoryboardSegue) {
+        // 他の画面から segue を使って戻ってきた時に呼ばれる
+    }
 
 }
 
---
+++ SecondKadaiApp/nameViewController.swift
@@ -0,0 +1,45 @@
+//
+//  nameViewController.swift
+//  SecondKadaiApp
+//
+//  Created by 足立咲季子 on 2018/07/22.
+//  Copyright © 2018年 sakiko.adachi. All rights reserved.
+//
+
+import UIKit
+
+class nameViewController: UIViewController {
+
+    @IBOutlet weak var namelabel: UILabel!
+    
+    var name: String?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        
+        let result = name
+        namelabel.text = "こんにちは、 \(String(describing: result)) さん"
+        
+    }
+
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
